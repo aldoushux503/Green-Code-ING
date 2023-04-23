88b5bf45aeb33df1fe7c5acb3550edc3a86a3369
aldoushux503 <elnikovnikita4@gmail.com>
2023-04-23 16:12:53 +0200

    Added a implementation for a Clan class

--- src/main/java/com/example/greencoding/onlinegame/Clan.java
+++ src/main/java/com/example/greencoding/onlinegame/Clan.java
@@ -1,4 +1,30 @@
 package com.example.greencoding.onlinegame;
 
 public class Clan {
+    private int numberOfPlayers;
+    private int points;
+
+    public Clan() {
+    }
+
+    public Clan(int numberOfPlayers, int points) {
+        this.numberOfPlayers = numberOfPlayers;
+        this.points = points;
+    }
+
+    public int getNumberOfPlayers() {
+        return numberOfPlayers;
+    }
+
+    public void setNumberOfPlayers(int numberOfPlayers) {
+        this.numberOfPlayers = numberOfPlayers;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
 }
