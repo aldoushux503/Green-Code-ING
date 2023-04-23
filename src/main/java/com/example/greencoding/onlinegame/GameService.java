c091d4cb7b7e6af3baa7c7bec361d80dbcecfab3
aldoushux503 <elnikovnikita4@gmail.com>
2023-04-23 18:10:36 +0200

    Fixed a problem with the wrong order of classes with the same number of points

--- src/main/java/com/example/greencoding/onlinegame/GameService.java
+++ src/main/java/com/example/greencoding/onlinegame/GameService.java
@@ -10,41 +10,59 @@ import java.util.List;
 
 @Service
 public class GameService {
-
-
     public ResponseEntity<List<List<Clan>>> calculateEntranceOrder(GameRequest gameRequest) {
         int groupCount = gameRequest.getGroupCount();
         List<Clan> clans = gameRequest.getClans();
 
-        // Sort clans by points in descending order
-        clans.sort(Comparator.comparingInt(Clan::getPoints).reversed());
+        // Validate input parameters
+        if (groupCount <= 0 || clans == null || clans.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        // Sort clans by points in descending order and then by number of players in ascending order
+        clans.sort(Comparator.comparingInt(Clan::getPoints).reversed()
+                .thenComparingInt(Clan::getNumberOfPlayers));
 
         List<List<Clan>> entranceOrder = new ArrayList<>();
 
         while (!clans.isEmpty()) {
-            List<Clan> group = new ArrayList<>();
-            int totalPlayersInGroup = 0;
-            int totalPointsInGroup = 0;
+            List<Clan> group = createGroup(clans, groupCount);
 
-            // Add as many clans as possible to the group
-            for (Clan clan : clans) {
-                if (clan.getNumberOfPlayers() + totalPlayersInGroup <= groupCount) {
-                    group.add(clan);
-                    totalPlayersInGroup += clan.getNumberOfPlayers();
-                    totalPointsInGroup += clan.getPoints();
-                }
+            if (group == null) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             }
 
-            // Remove clans from the list
             clans.removeAll(group);
 
-            // Sort the group by the number of players in ascending order, then by points in descending order
-            group.sort(Comparator.comparingInt(Clan::getNumberOfPlayers).thenComparingInt(Clan::getPoints).reversed());
-
-            // Add the group to the entrance order
             entranceOrder.add(group);
         }
 
         return new ResponseEntity<>(entranceOrder, HttpStatus.OK);
+    }
+
+    private List<Clan> createGroup(List<Clan> clans, int groupCount) {
+        List<Clan> group = new ArrayList<>();
+        int totalPlayersInGroup = 0;
+
+        for (Clan clan : clans) {
+            if (clan.getNumberOfPlayers() + totalPlayersInGroup <= groupCount) {
+                group.add(clan);
+                totalPlayersInGroup += clan.getNumberOfPlayers();
+            }
+
+            if (totalPlayersInGroup == groupCount) {
+                break;
+            }
+        }
+
+        if (group.isEmpty()) {
+            return null;
+        }
+
+        // Sort the group by number of players in ascending order and then by points in descending order
+        group.sort(Comparator.comparingInt(Clan::getPoints).reversed()
+                .thenComparingInt(Clan::getNumberOfPlayers));
+
+        return group;
     }
 }
