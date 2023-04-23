a12c9ccb622fc08791b48b0ece0e4b7884c90501
aldoushux503 <elnikovnikita4@gmail.com>
2023-04-23 16:22:46 +0200

    Added implementation to GameController and Game Service class

--- src/main/java/com/example/greencoding/onlinegame/GameController.java
+++ src/main/java/com/example/greencoding/onlinegame/GameController.java
@@ -1,5 +1,6 @@
 package com.example.greencoding.onlinegame;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,9 +11,16 @@ import java.util.List;
 @RestController
 public class GameController {
 
+    GameService gameService;
+
+    @Autowired
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
     @PostMapping("/onlinegame/calculate")
     public ResponseEntity<List<List<Clan>>> getCalculatedEntranceOrder(@RequestBody GameRequest gameRequest) {
-        return new ResponseEntity<>(null);
+        return gameService.calculateEntranceOrder(gameRequest);
     }
 
 }
