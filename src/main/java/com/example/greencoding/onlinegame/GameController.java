9dbb9dd22542bff6db491b78ba2ab6c5ae6fbb5a
aldoushux503 <elnikovnikita4@gmail.com>
2023-04-23 16:16:57 +0200

    Added an implementation for a GameRequest class

--- src/main/java/com/example/greencoding/onlinegame/GameController.java
+++ src/main/java/com/example/greencoding/onlinegame/GameController.java
@@ -1,8 +1,18 @@
 package com.example.greencoding.onlinegame;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class GameController {
+
+    @PostMapping("/onlinegame/calculate")
+    public ResponseEntity<List<List<Clan>>> getCalculatedEntranceOrder(@RequestBody GameRequest gameRequest) {
+        return new ResponseEntity<>(null);
+    }
 
 }
--- src/main/java/com/example/greencoding/onlinegame/GameRequest.java
+++ src/main/java/com/example/greencoding/onlinegame/GameRequest.java
@@ -1,4 +1,32 @@
 package com.example.greencoding.onlinegame;
 
+import java.util.List;
+
 public class GameRequest {
+    private int groupCount;
+    private List<Clan> clans;
+
+    public GameRequest() {
+    }
+
+    public GameRequest(int groupCount, List<Clan> clans) {
+        this.groupCount = groupCount;
+        this.clans = clans;
+    }
+
+    public int getGroupCount() {
+        return groupCount;
+    }
+
+    public void setGroupCount(int groupCount) {
+        this.groupCount = groupCount;
+    }
+
+    public List<Clan> getClans() {
+        return clans;
+    }
+
+    public void setClans(List<Clan> clans) {
+        this.clans = clans;
+    }
 }
