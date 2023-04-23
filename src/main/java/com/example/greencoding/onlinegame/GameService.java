feac8e85cf8331b7a162de85900e64f7e8bb7a43
aldoushux503 <elnikovnikita4@gmail.com>
2023-04-23 16:21:52 +0200

    Added implementation to GameController and Game Service class

--- src/main/java/com/example/greencoding/onlinegame/GameService.java
+++ src/main/java/com/example/greencoding/onlinegame/GameService.java
@@ -1,4 +1,50 @@
 package com.example.greencoding.onlinegame;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+@Service
 public class GameService {
+
+
+    public ResponseEntity<List<List<Clan>>> calculateEntranceOrder(GameRequest gameRequest) {
+        int groupCount = gameRequest.getGroupCount();
+        List<Clan> clans = gameRequest.getClans();
+
+        // Sort clans by points in descending order
+        clans.sort(Comparator.comparingInt(Clan::getPoints).reversed());
+
+        List<List<Clan>> entranceOrder = new ArrayList<>();
+
+        while (!clans.isEmpty()) {
+            List<Clan> group = new ArrayList<>();
+            int totalPlayersInGroup = 0;
+            int totalPointsInGroup = 0;
+
+            // Add as many clans as possible to the group
+            for (Clan clan : clans) {
+                if (clan.getNumberOfPlayers() + totalPlayersInGroup <= groupCount) {
+                    group.add(clan);
+                    totalPlayersInGroup += clan.getNumberOfPlayers();
+                    totalPointsInGroup += clan.getPoints();
+                }
+            }
+
+            // Remove clans from the list
+            clans.removeAll(group);
+
+            // Sort the group by the number of players in ascending order, then by points in descending order
+            group.sort(Comparator.comparingInt(Clan::getNumberOfPlayers).thenComparingInt(Clan::getPoints).reversed());
+
+            // Add the group to the entrance order
+            entranceOrder.add(group);
+        }
+
+        return new ResponseEntity<>(entranceOrder, HttpStatus.OK);
+    }
 }
