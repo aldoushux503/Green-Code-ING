77a128d4c54518c45185fd997ece1608067621b4
aldoushux503 <elnikovnikita4@gmail.com>
2023-04-23 20:34:44 +0200

    Added a implementation to a Account class

--- src/main/java/com/example/greencoding/transactions/Transaction.java
+++ src/main/java/com/example/greencoding/transactions/Transaction.java
@@ -3,5 +3,31 @@ package com.example.greencoding.transactions;
 import java.math.BigDecimal;
 
 public class Transaction {
+    private String debitAccount;
+    private String creditAccount;
+    private BigDecimal amount;
 
+    public String getDebitAccount() {
+        return debitAccount;
+    }
+
+    public void setDebitAccount(String debitAccount) {
+        this.debitAccount = debitAccount;
+    }
+
+    public String getCreditAccount() {
+        return creditAccount;
+    }
+
+    public void setCreditAccount(String creditAccount) {
+        this.creditAccount = creditAccount;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
 }
