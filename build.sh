aec611fcec9079aecfc841bab840697ebeb304e9
aldoushux503 <elnikovnikita4@gmail.com>
2023-04-27 15:14:32 +0200

    Added a build.sh and a run.sh

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Compile the code
+mvn clean package
---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Set the working directory
+cd target
+
+# Start the application
+java -jar GreenCoding-0.0.1-SNAPSHOT.jar
