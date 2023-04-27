d58d36a0e8b0c337ff1361d564afaffaa2d6b8f6
aldoushux503 <elnikovnikita4@gmail.com>
2023-04-27 10:00:21 +0200

    Replaced an algorithm with a more efficient one at a calculateOrder method

--- src/main/java/com/example/greencoding/atmservice/AtmService.java
+++ src/main/java/com/example/greencoding/atmservice/AtmService.java
@@ -10,57 +10,41 @@ import java.util.*;
 public class AtmService {
 
     public ResponseEntity<List<Atm>> calculateOrder(List<Task> tasks) {
-        // Sort tasks by region number
-        tasks.sort(Comparator.comparing(Task::getRegion));
-        // Create a map to store tasks by region number
-        Map<Integer, List<Task>> regionMap = new HashMap<>();
-        // To avoid duplicate tasks in the result
+        // Use a more memory-efficient data structure for regionMap
+        Map<Integer, LinkedList<Task>> regionMap = new HashMap<>();
+        final int[] maxRegion = {0};
         Set<Atm> addedAtms = new TreeSet<>(Comparator.comparing(Atm::getRegion).thenComparing(Atm::getAtmId));
 
-        int maxRegion = 0;
-        for (Task task : tasks) {
-            int region = task.getRegion();
-            maxRegion = Math.max(region, maxRegion);
-
+        // Use parallel processing to sort the tasks by region number
+        tasks.parallelStream().sorted(Comparator.comparing(Task::getRegion)).forEach(task -> {
             Atm atm = new Atm(task.getRegion(), task.getAtmId());
             if (addedAtms.add(atm)) {
-                regionMap.computeIfAbsent(region, k -> new ArrayList<>()).add(task);
+                int region = task.getRegion();
+                maxRegion[0] = Math.max(region, maxRegion[0]);
+                regionMap.computeIfAbsent(region, k -> new LinkedList<>()).add(task);
             }
-        }
+        });
 
+        // Use a single loop to handle all the tasks
         List<Atm> result = new ArrayList<>();
-        for (int region = 1; region <= maxRegion; region++) {
+        for (int region = 1; region <= maxRegion[0]; region++) {
             if (!regionMap.containsKey(region)) {
                 continue;
             }
-            List<Task> regionTasks = regionMap.get(region);
-            List<Task> priorityTasks = new ArrayList<>();
-            List<Task> standardTasks = new ArrayList<>();
-            Task failureRestartTask = null;
-            Task signalLowTask = null;
-            for (Task task : regionTasks) {
-                if (task.getRequestType() == RequestType.PRIORITY) {
-                    priorityTasks.add(task);
-                } else if (task.getRequestType() == RequestType.STANDARD) {
-                    standardTasks.add(task);
-                } else if (task.getRequestType() == RequestType.FAILURE_RESTART) {
-                    failureRestartTask = task;
-                } else {
-                    signalLowTask = task;
+            LinkedList<Task> regionTasks = regionMap.get(region);
+
+            // Use parallel processing to handle the tasks
+            regionTasks.parallelStream().forEach(task -> {
+                if (task.getRequestType() == RequestType.FAILURE_RESTART) {
+                    result.add(new Atm(task.getRegion(), task.getAtmId()));
                 }
-            }
-            // Handle the failure restart task first, if present
-            if (failureRestartTask != null) {
-                result.add(new Atm(failureRestartTask.getRegion(), failureRestartTask.getAtmId()));
-            }
-            // Handle the priority tasks next
-            priorityTasks.forEach(task -> result.add(new Atm(task.getRegion(), task.getAtmId())));
-            // Handle the signal low task, if present, after the priority tasks
-            if (signalLowTask != null) {
-                result.add(new Atm(signalLowTask.getRegion(), signalLowTask.getAtmId()));
-            }
-            // Handle the standard tasks last
-            standardTasks.forEach(task -> result.add(new Atm(task.getRegion(), task.getAtmId())));
+            });
+            regionTasks.parallelStream().filter(task -> task.getRequestType() == RequestType.PRIORITY)
+                    .forEach(task -> result.add(new Atm(task.getRegion(), task.getAtmId())));
+            regionTasks.parallelStream().filter(task -> task.getRequestType() == RequestType.SIGNAL_LOW)
+                    .forEach(task -> result.add(new Atm(task.getRegion(), task.getAtmId())));
+            regionTasks.parallelStream().filter(task -> task.getRequestType() == RequestType.STANDARD)
+                    .forEach(task -> result.add(new Atm(task.getRegion(), task.getAtmId())));
         }
 
         return ResponseEntity.ok(result);
